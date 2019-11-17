9114faed907d6452b1bbb2f8995c30562207dba4
Guilherme Ottoni <guiottonie@gmail.com>
2019-11-17 17:53:50 -0300

    termina código do serviço 1

---
+++ ServiceOneApi/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sone;
+
+namespace ServiceOneApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{productid}/{userid}")]
+        public async Task<ActionResult<Product>> Get(string productid, string userid)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:50051");
+            var client = new ServiceOne.ServiceOneClient(channel);
+
+            var reply = (await Task.Run(() => { return client.ProductDiscount(new ProductRequest { ProductId = productid, UserId = userid }); }));
+
+            return Ok(reply);
+        }
+
+
+    }
+}
